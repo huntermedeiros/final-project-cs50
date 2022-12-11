6dde100061cae8ed84e758a87f29b7edc286ba63
Hunter Medeiros <88hunter.medeiros88@gmail.com>
2022-12-10 17:06:33 -0800

    start of backend

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
+# Configure Flask App
 app = Flask(__name__)
+
+# Configure Session
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", homeActive = True)
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", profileActive = True)
+
+@app.route("/friends")
+def friends():
+    return render_template("friends.html", friendsActive = True)
+
+@app.route("/liked")
+def liked():
+    return render_template("liked.html", likedActive = True)
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html", settingsActive = True)
+
+@app.route("/login")
+def login():
+
+    session.clear()
+    
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/post")
+def post():
+    return render_template("post.html")
 
 @app.route("/search")
 def search():
     return render_template("search.html")
-
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
-
-@app.route("/friends")
-def friends():
-    return render_template("friends.html")
-
-@app.route("/liked")
-def liked():
-    return render_template("liked.html")
-
-
-
     
--- static/scripts.js
+++ static/scripts.js
@@ -4,18 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const postButton = document.querySelector(".post-button");
     const postButtonText = document.querySelector(".post-button-text");
     const sideNavText = document.querySelectorAll(".side-nav-text");
-    const sideNavButton = document.querySelectorAll(".side-nav-button");
+    const sideNavButtons = document.querySelectorAll(".side-nav-button");
 
     // Expands and shrinks the side nav bar
-    var sideNavState = true;
+    var sideNavState = false;
     menuButton.addEventListener('click', function() {
         if (sideNavState == true) {
             sideNavState = false;
-
             postButton.style.width = '40px';
             postButtonText.style.display = 'none';
-            for (let i = 0; i < sideNavButton.length; i++) {
-                sideNavButton[i].style.width = '40px';
+            for (let i = 0; i < sideNavButtons.length; i++) {
+                sideNavButtons[i].style.width = '40px';
             }
             for (let i = 0; i < sideNavText.length; i++) {
                 sideNavText[i].style.display = 'none';
@@ -26,8 +25,8 @@ document.addEventListener('DOMContentLoaded', function() {
             sideNavState = true;
             postButton.style.width = '140px';
             postButtonText.style.display = 'inline';
-            for (let i = 0; i < sideNavButton.length; i++) {
-                sideNavButton[i].style.width = '140px';
+            for (let i = 0; i < sideNavButtons.length; i++) {
+                sideNavButtons[i].style.width = '140px';
             }
             for (let i = 0; i < sideNavText.length; i++) {
                 sideNavText[i].style.display = 'inline';
